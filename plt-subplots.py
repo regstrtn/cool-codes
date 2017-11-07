e91a00a22d5f2bb034b8bc12d9c3bde6798c1e5b
regstrtn <regstrtn@gmail.com>
2017-11-07 17:35:09 +0530

    Update plt-subplots.py

--- plt-subplots.py
+++ plt-subplots.py
@@ -11,7 +11,9 @@ x2 = np.linspace(0.0, 2.0)
 y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
 y2 = np.cos(2 * np.pi * x2)
 
-plt.subplot(2, 1, 1)
+plt.figure(figsize = (10, 6)) #figsize = (width, height)
+
+plt.subplot(2, 1, 1)  #Create 2 rows and 1 column. Plotting first plot)
 plt.plot(x1, y1, 'ko-')
 plt.title('A tale of 2 subplots')
 plt.ylabel('Damped oscillation')
